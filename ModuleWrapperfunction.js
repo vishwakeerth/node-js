b5666a2f4228de1e6ba5bb347515ec90b0977cd9
vishwa keerth <101543201+vishwakeerth@users.noreply.github.com>
2022-06-24 15:31:53 +0530

    Update ModuleWrapperfunction.js

--- ModuleWrapperfunction.js
+++ ModuleWrapperfunction.js
@@ -1,11 +1,11 @@
 //(function(expotrs, require, module, __filename, __dirname){
 
-kiran = {
-    name:"Kiran",
+vishwa = {
+    name:"vishwa",
     favnum : 7,
     Comp : "Capgemini"
 }
 console.log(exports, require, module, __filename, __dirname)
-module.exports = kiran;
+module.exports = vishwa;
 
 // })
